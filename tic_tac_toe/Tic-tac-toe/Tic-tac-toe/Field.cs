03cbbcf9acf3b601c97316d70b26ba9ecb672515
ntlmir <49791963+ntlmir@users.noreply.github.com>
2020-10-02 20:01:02 +0500

    Add files via upload

---
+++ tic_tac_toe/Tic-tac-toe/Tic-tac-toe/Field.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tac_toe
+{
+    class Field
+    {
+        private string[,] field = new string[3, 3] { { "_", "_", "_" }, { "_", "_", "_" }, { "_", "_", "_" } };
+        private bool lastPlayer;
+        private int countcells;
+        public Field()
+        {
+            lastPlayer = false;
+            countcells = 0;
+        }
+
+        public void PrintField()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(field[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void DoStep(bool player, int x, int y)
+        {
+            if(lastPlayer != player && countcells != 9)
+            {
+                if (field[x, y] == "_")
+                {
+                    if (countcells != 9)
+                    {
+                        switch (player)
+                        {
+                            case true:
+                                field[x, y] = "X";
+                                countcells++;
+                                if (CheckWinners() == true)
+                                {
+                                    countcells = 9;
+                                    Console.WriteLine("Х is winner! End of the Game.");
+                                    PrintField();
+                                }
+                                break;
+                            case false:
+                                field[x, y] = "O";
+                                countcells++;
+                                if (CheckWinners() == true)
+                                {
+                                    countcells = 9;
+                                    Console.WriteLine("Y is winner! End of the Game.");
+                                    PrintField();
+                                }
+                                break;
+                        }
+                        lastPlayer = player;
+                        if (countcells == 9 && CheckWinners() != true)
+                        {
+                            Console.WriteLine("The Field is full. End of the Game.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The Field is full. End of the Game.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Can't do that step, choose another place");
+                }
+            }
+            else if(countcells == 9)
+            {
+                Console.WriteLine("Game is over.");
+            }
+            else
+            {
+                Console.WriteLine("Not your step");
+            }
+        }
+
+        public bool CheckWinners()
+        {
+            for(int i=0; i<3; i++)
+            {
+                if (field[i, 0] == field[i, 1] && field[i, 0] == field[0, 2] && field[i, 0] != "_")
+                {
+                    return true;
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[0, i] == field[1, i] && field[0, i] == field[2, i] && field[0, i] != "_")
+                {
+                    return true;
+                }
+            }
+            if (field[0, 0] == field[1, 1] && field[0, 0] == field[2, 2] && field[1, 1] != "_")
+            {
+                return true;
+            }
+            else if (field[0, 2] == field[1, 1] && field[0, 2] == field[2, 0] && field[1, 1] != "_")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tic_tac_toe/Tic-tac-toe/Tic-tac-toe/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tac_toe
+{
+    class Program
+    {
+//        Написать класс для игры в крестики-нолики: поле 3x3, два игрока по очереди ставят крестик и нолик.
+//      Игра должна проверять корректность действий:
+//      Не давать крестику или нолику сходить не в свой ход
+//      Не давать сходить в занятую клетку
+//      Не давать сделать ход после завершения игры
+//      Игра должна иметь минимальный необходимый интерфейс и не давать сломать свои данные снаружи.
+        static void Main(string[] args)
+        {
+            Field toe = new Field();
+
+            toe.PrintField();
+            Console.WriteLine();
+
+            bool playerOne = true; //X
+            bool playerTwo = false; //Y
+
+            toe.DoStep(playerOne, 0, 0);
+            //X _ _
+            //_ _ _
+            //_ _ _
+            toe.DoStep(playerTwo, 1, 1);
+            //X _ _
+            //_ O _
+            //_ _ _
+            toe.DoStep(playerOne, 2, 2);
+            //X _ _
+            //_ O _
+            //_ _ X
+            toe.DoStep(playerTwo, 2, 0);
+            //X _ _
+            //_ O _
+            //O _ X
+            toe.DoStep(playerOne, 0, 2);
+            //X _ X
+            //_ O _
+            //O _ X
+            toe.DoStep(playerTwo, 1, 2);
+            //X _ X
+            //_ O O
+            //O _ X
+            toe.DoStep(playerOne, 0, 1);
+            //X X X
+            //_ O O
+            //O _ X
+            //toe.PrintField();
+
+
+            Console.ReadLine();
+        }
+    }
+}
