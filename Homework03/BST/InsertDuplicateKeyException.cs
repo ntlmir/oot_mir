b9a7063e33f52b31638a2870907d480a030a9d0f
ntlmir <49791963+ntlmir@users.noreply.github.com>
2021-01-28 00:12:21 +0500

    Update InsertDuplicateKeyException.cs

--- Homework03/BST/InsertDuplicateKeyException.cs
+++ Homework03/BST/InsertDuplicateKeyException.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BST
 {
-    class InsertDuplicateKeyException : Exception
+    public class InsertDuplicateKeyException : Exception
     {
         public InsertDuplicateKeyException() { }
         public InsertDuplicateKeyException(string key)
